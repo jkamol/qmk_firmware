f36b485c9b7861f717586c23cd0c47bc6f8a5538
Kamol Jutiwattananukul <jkamol@gmail.com>
2023-12-14 10:48:06 +0700

    Change to use num dot at sym layer

--- keyboards/a_dux/keymaps/jkamol/keymap.c
+++ keyboards/a_dux/keymaps/jkamol/keymap.c
@@ -154,7 +154,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_SYM] = LAYOUT_split_3x5_2(
         TD(DANCE_QUIT), KC_7,           TD(DANCE_TASK), KC_9,           KC_0,               /**/ KC_CIRCUMFLEX,     KC_AMPERSAND,   KC_AT,          KC_HASH,        KC_DOLLAR,
         LSFT_T(KC_1),   KC_2,           KC_3,           KC_4,           KC_5,               /**/ KC_GRAVE,          KC_MINUS,       KC_ASTERISK,    KC_EQUAL,       LSFT_T(KC_SCLN),
-        RCTL_T(KC_DOT), KC_BSPC,        TD(DANCE_SCR),  KC_DEL,         QK_REP,             /**/ KC_PERCENT,        KC_LBRC,        KC_BACKSLASH,   KC_EXCLAIM,     RCTL_T(KC_RBRC),
+        RCTL_T(KC_KP_DOT), KC_BSPC,     TD(DANCE_SCR),  KC_DEL,         QK_REP,             /**/ KC_PERCENT,        KC_LBRC,        KC_BACKSLASH,   KC_EXCLAIM,     RCTL_T(KC_RBRC),
                                                         MO(_FN),        XXXXXXX,            /**/ MO(_MACRO),        _______
     ),
     [_NAV] = LAYOUT_split_3x5_2(
